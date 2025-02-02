e18266b6d32512d61c7589e19b2b1a5ed2744d18
BugHunter-X <askzenai@gmail.com>
2025-02-02 15:56:05 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+function About(){
+    return(
+        <h1>About page</h1>
+    );
+}
+export default About;
+```
